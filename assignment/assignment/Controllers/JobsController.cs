97c409241fecdd05c0db4ea90295b6196093ebc5
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-30 00:41:41 -0800

    Added models for Services and jobs. Also got the CRUD done for services. Finish Create for Jobs

---
+++ assignment/assignment/Controllers/JobsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using assignment.Models;
+
+namespace assignment.Controllers
+{
+    public class JobsController : Controller
+    {
+        private PersonContext db = new PersonContext();
+
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            var jobs = db.Jobs.Include(j => j.Customer).Include(j => j.Service);
+            return View(jobs.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Address");
+            ViewBag.ServiceId = new SelectList(db.Services, "ServiceID", "Name");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ServiceId,CustomerId")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(job);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Address", job.CustomerId);
+            ViewBag.ServiceId = new SelectList(db.Services, "ServiceID", "Name", job.ServiceId);
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Console.WriteLine("ID: " + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Address", job.CustomerId);
+            ViewBag.ServiceId = new SelectList(db.Services, "ServiceID", "Name", job.ServiceId);
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ServiceId,CustomerId")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Address", job.CustomerId);
+            ViewBag.ServiceId = new SelectList(db.Services, "ServiceID", "Name", job.ServiceId);
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Job job = db.Jobs.Find(id);
+            db.Jobs.Remove(job);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- assignment/assignment/Models/Person.cs
+++ assignment/assignment/Models/Person.cs
@@ -16,11 +16,33 @@ namespace assignment.Models
     {
        public string Address { get; set; }
 
+        public virtual ICollection<Job> Job { get;set; }
+
     }
 
     public class Employee : Person
     {
         public string Department { get; set; }
+        public virtual ICollection<Service> Services { get; set; }
+    }
+
+
+    public class Service
+    {
+        public int ServiceID { get; set; }
+        public string Name { get; set; }
+        public int EmployeeID { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public virtual ICollection<Job> Job { get; set; }
+    }
+
+    public class Job
+    {
+        public int CustomerId { get; set; }
+        public int ServiceId { get; set; }
+        public virtual Service Service { get; set; }
+        public virtual Customer Customer { get; set; }
     }
 
     public class PersonContext : DbContext
@@ -28,12 +50,37 @@ namespace assignment.Models
         public DbSet<Person> People { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Service> Services { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            // Derived Tables using TPT or Table Per Type
             modelBuilder.Entity<Customer>().ToTable("Customer");
             modelBuilder.Entity<Employee>().ToTable("Employee");
+
+            // Employee -> Service
+            modelBuilder.Entity<Service>()
+                .HasRequired(s => s.Employee)
+                .WithMany(e => e.Services)
+                .HasForeignKey(s => s.EmployeeID);
+
+
+            // Service -> Job <- Customer
+            modelBuilder.Entity<Job>()
+                .HasKey(sc => new { sc.ServiceId, sc.CustomerId });
+
+            modelBuilder.Entity<Job>()
+                .HasRequired(job => job.Service)
+                .WithMany(service => service.Job)
+                .HasForeignKey(job => job.ServiceId);
+
+            modelBuilder.Entity<Job>()
+                .HasRequired(job => job.Customer)
+                .WithMany(customer => customer.Job)
+                .HasForeignKey(job => job.CustomerId);
         }
+
+        public System.Data.Entity.DbSet<assignment.Models.Job> Jobs { get; set; }
     }
 
 
