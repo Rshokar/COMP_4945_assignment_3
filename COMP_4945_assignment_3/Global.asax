6cc924c3adfac21b05bfb5ee34d5dd66f6f2a4ed
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-29 23:44:06 -0800

    Created Customer and Employee CRUD

--- COMP_4945_assignment_3/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP_4945_assignment_3.MvcApplication" Language="C#" %>
---
+++ assignment/assignment/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace assignment.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Customer : Person
+    {
+       public string Address { get; set; }
+
+    }
+
+    public class Employee : Person
+    {
+        public string Department { get; set; }
+    }
+
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+    }
+
+
+}
