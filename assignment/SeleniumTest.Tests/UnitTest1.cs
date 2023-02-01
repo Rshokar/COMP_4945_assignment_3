4323126ace6415e248c18c7780e2b48106bcd241
Monica Bacatan <monicab3002@outlook.com>
2023-02-01 15:08:10 -0800

    Added gitignore for mdf file and added another test

--- assignment/SeleniumTest.Tests/UnitTest1.cs
+++ assignment/SeleniumTest.Tests/UnitTest1.cs
@@ -24,5 +24,21 @@ namespace SeleniumTest.Tests
             driver.FindElement(By.Id("Address")).SendKeys("Springfield");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // This method tests creating a new employee
+            string url = "https://localhost:44309//employees";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Test");
+            driver.FindElement(By.Id("Department")).SendKeys("Computing");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
     }
 }
