4e80b4306f8643ea8850d6094cc1ee21b8b8fa87
Monica Bacatan <monicab3002@outlook.com>
2023-02-01 22:33:35 -0800

    Added tests for editing customers, employees, and services

--- assignment/SeleniumTest.Tests/UnitTest1.cs
+++ assignment/SeleniumTest.Tests/UnitTest1.cs
@@ -62,5 +62,88 @@ namespace SeleniumTest.Tests
 
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
         }
+
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // This method tests editing a customer
+            string url = "https://localhost:44309//customers";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Edit")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Address")).Clear();
+
+            driver.FindElement(By.Id("Name")).SendKeys("EditTest");
+            driver.FindElement(By.Id("Address")).SendKeys("EditTest");
+
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            // This method tests editing an employee
+            string url = "https://localhost:44309//employees";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Edit")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Department")).Clear();
+
+            driver.FindElement(By.Id("Name")).SendKeys("EditTest");
+            driver.FindElement(By.Id("Department")).SendKeys("EditTest");
+
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            // This method tests editing a service
+            string url = "https://localhost:44309//services";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Name")).SendKeys("EditTest");
+
+            // Select the dropdown option with the name test (this user was set in a previous test)
+            SelectElement dropDown = new SelectElement(driver.FindElement(By.Id("EmployeeID")));
+            dropDown.SelectByText("EditTest");
+
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+
+        // ***NOT WORKING
+        [TestMethod]
+        public void TestMethod7()
+        {
+            // This method tests deleting a customer
+            string url = "https://localhost:44309//customers";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Delete")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
     }
 }
